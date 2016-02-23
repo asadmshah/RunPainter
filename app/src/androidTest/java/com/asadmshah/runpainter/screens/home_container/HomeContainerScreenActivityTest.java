9a59c61437ba7018c48d343088c5752bd43f059a
Asad Shah <me@asadmshah.com>
2016-02-23 19:52:32 +0500

    Fixes basics for instrumentation tests in order to... work

--- app/src/androidTest/java/com/asadmshah/runpainter/screens/home_container/HomeContainerScreenActivityTest.java
+++ app/src/androidTest/java/com/asadmshah/runpainter/screens/home_container/HomeContainerScreenActivityTest.java
@@ -4,9 +4,6 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
-import com.asadmshah.runpainter.utils.DisableAnimationsRule;
-
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,9 +11,6 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class HomeContainerScreenActivityTest {
-
-    @ClassRule
-    public static DisableAnimationsRule disableAnimationsRule = new DisableAnimationsRule();
 
     @Rule
     public ActivityTestRule<HomeContainerScreenActivity> activityTestRule = new ActivityTestRule<>(HomeContainerScreenActivity.class, true, true);
--- app/src/androidTest/java/com/asadmshah/runpainter/screens/settings_container/SettingsContainerScreenActivityTest.java
+++ app/src/androidTest/java/com/asadmshah/runpainter/screens/settings_container/SettingsContainerScreenActivityTest.java
@@ -4,9 +4,6 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
-import com.asadmshah.runpainter.utils.DisableAnimationsRule;
-
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,9 +11,6 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class SettingsContainerScreenActivityTest {
-
-    @ClassRule
-    public static DisableAnimationsRule disableAnimationsRule = new DisableAnimationsRule();
 
     @Rule
     public ActivityTestRule<SettingsContainerScreenActivity> activityTestRule = new ActivityTestRule<>(SettingsContainerScreenActivity.class, true, true);
